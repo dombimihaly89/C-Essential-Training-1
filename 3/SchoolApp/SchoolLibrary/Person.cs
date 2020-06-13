2648c0a3a20fa94f2db7db4cb309f97ed88f173b
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-13 19:19:22 +0200

    Made an abstract class and also abstract method. Inherited from abstract class and implementes abstract method in subclasses. Also tested it in our form app

---
+++ 3/SchoolApp/SchoolLibrary/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        // We can put abstract methods not just only in absctract classes.
+        // You have to implement abstract methods in child classes.
+        public abstract float ComputeGradeAverage();
+    }
+}
